6982927bb71152d1ac881aea19ef33f966d8c7fe
burnyst <64452384+burnyst@users.noreply.github.com>
2020-05-13 18:02:21 +0900

    검색어 관련 수정
    포커스 처리
    검색어 valid 검사

--- src/WebContent/js/search.js
+++ src/WebContent/js/search.js
@@ -1,18 +1,32 @@
 window.addEventListener("load", function() {
+	var form = document.querySelectorAll("div.search form");
+	if (form[0]["productName"].value == "") {
+		form[0]["productName"].focus();
+	}
+	form[0].addEventListener("submit", function() {
+		if (form[0]["productName"].value.trim() == "") {
+			form[0]["productName"].value = "";
+			return false;
+		} else {
+			return true;
+		}
+	});
     var table = document.querySelectorAll("table.items");
-    var len = table[0].rows.length;
-    for (var i = 1; i < len; i+=2) {
-        table[0].rows[i].addEventListener("click", function() {
-            var desc = this.nextSibling.nextSibling;
-            var className = desc.className;
-            var table = document.querySelectorAll("table.items");
-            var len = table[0].rows.length;
-            for (var i = 2; i < len; i+=2) {
-                table[0].rows[i].className = "hidden";
-            }
-            if ("hidden" === className) {
-                desc.className = "";
-            }
-        }, false);
+    if (table.length > 0) {
+	    var len = table[0].rows.length;
+	    for (var i = 1; i < len; i+=2) {
+	        table[0].rows[i].addEventListener("click", function() {
+	            var desc = this.nextSibling.nextSibling;
+	            var className = desc.className;
+	            var table = document.querySelectorAll("table.items");
+	            var len = table[0].rows.length;
+	            for (var i = 2; i < len; i+=2) {
+	                table[0].rows[i].className = "hidden";
+	            }
+	            if ("hidden" === className) {
+	                desc.className = "";
+	            }
+	        });
+	    }
     }
 });
