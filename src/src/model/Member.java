99291cad4920e2504649850ead7ec59d4604e66f
arumhongg <64457585+arumhongg@users.noreply.github.com>
2020-05-21 14:18:53 +0900

    member.java update

--- src/src/model/Member.java
+++ src/src/model/Member.java
@@ -6,16 +6,17 @@ public class Member {
 	private String	mid;
 	private String	mname;
 	private String	mpwd;
+	private String	mpwd2;
 	private String	memail;
 	private String	mhp;
 	private String	mbd;
 	private int		mlevel;
-	private int 	xmember;
+	private int		xmember;
 	private String	xreason;
 	
 	//생성자
 	public Member(String mid, String mname, String mpwd, String memail,String mhp,
-			String mbd, int mlevel, int xmember, String xreason) {
+			String mbd, int mlevel, char xmember, String xreason) {
 		this.mid	=	mid;
 		this.mname	=	mname;
 		this.mpwd	=	mpwd;
@@ -27,7 +28,14 @@ public class Member {
 		this.xreason=	xreason;
 	}
 	
-	public Member(String mid, String mname, String mpwd, String memail, String mhp, String mbd, int mlevel) {
+	@Override
+	public String toString() {
+		return "Member [mid=" + mid + ", mname=" + mname + ", mpwd=" + mpwd + ", mpwd2=" + mpwd2 + ", memail=" + memail
+				+ ", mhp=" + mhp + ", mbd=" + mbd + ", mlevel=" + mlevel + ", xmember=" + xmember + ", xreason="
+				+ xreason + "]";
+	}
+
+	public Member(String mid, String mname, String mpwd,String memail, String mhp, String mbd, int mlevel) {
 		this.mid	=	mid;
 		this.mname	=	mname;
 		this.mpwd	=	mpwd;
@@ -44,6 +52,17 @@ public class Member {
 		this.memail	=	memail;
 		this.mhp	=	mhp;
 		this.mbd	=	mbd;
+	}
+	
+	//MyinfoUpdateService용
+	public Member(String mpwd, String mpwd2, String memail, String mhp) {
+		this.mpwd	=	mpwd;
+		this.mpwd2	=	mpwd2;
+		this.memail	=	memail;
+		this.mhp	=	mhp;
+	}
+
+	public Member() {
 	}
 
 	//getters&setters
@@ -69,6 +88,14 @@ public class Member {
 
 	public void setMpwd(String mpwd) {
 		this.mpwd = mpwd;
+	}
+	
+	public String getMpwd2() {
+		return mpwd2;
+	}
+
+	public void setMpwd2(String mpwd2) {
+		this.mpwd2 = mpwd2;
 	}
 
 	public String getMemail() {
@@ -102,8 +129,6 @@ public class Member {
 	public void setMlevel(int mlevel) {
 		this.mlevel = mlevel;
 	}
-
-	
 
 	public int getXmember() {
 		return xmember;
